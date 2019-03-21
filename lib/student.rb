bdcef691913ced3f014469d1c8dd14264c4df5ab
Serena Berry <siberry226@gmail.com>
2019-03-21 13:10:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,16 @@
 class Student
-
-  attr_reader :id
   attr_accessor :name, :grade
-  
+  attr_reader :id
+
   def initialize(name, grade, id=nil)
     @id = id
     @name = name
     @grade = grade
   end
-  
+
   def self.create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade TEXT
@@ -19,28 +18,71 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
-  
+
   def self.drop_table
-    sql = <<-SQL
-      DROP TABLE students
-      SQL
+    sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name,grade)
-      VALUES (?,?)
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
     SQL
-    
+
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
-  
+
   def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
     student
   end
-  
 end
+# class Student
+
+#   attr_reader :id
+#   attr_accessor :name, :grade
+  
+#   def initialize(name, grade, id=nil)
+#     @id = id
+#     @name = name
+#     @grade = grade
+#   end
+  
+#   def self.create_table
+#     sql = <<-SQL
+#       CREATE TABLE IF NOT EXISTS songs (
+#         id INTEGER PRIMARY KEY,
+#         name TEXT,
+#         grade TEXT
+#         )
+#     SQL
+#     DB[:conn].execute(sql)
+#   end
+  
+#   def self.drop_table
+#     sql = <<-SQL
+#       DROP TABLE students
+#       SQL
+#     DB[:conn].execute(sql)
+#   end
+  
+#   def save
+#     sql = <<-SQL
+#       INSERT INTO students (name,grade)
+#       VALUES (?,?)
+#     SQL
+    
+#     DB[:conn].execute(sql, self.name, self.grade)
+#     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+#   end
+  
+#   def self.create(name:, grade:)
+#     student = Student.new(name, grade)
+#     student.save
+#     student
+#   end
+  
+# end
